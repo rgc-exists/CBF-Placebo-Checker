9c6b724e9a14f32eba7eff488da79e20fc3f42be
rgc-exists <103580754+rgc-exists@users.noreply.github.com>
2025-06-05 22:29:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CBF Placebo Checker
+Adds a button to the main menu that either turns CBF on or off, without telling you which.
+
+# Why?
+This mod can be used to find out if CBF is truly helping you, or if it's just the Placebo Effect.
+
+# What is the Placebo Effect?
+The Placebo Effect is originally a term used for a medication that doesn't actually do anything, but still has an effect on the person taking it because they THINK it will do something.  
+It can also be applied to other things, though. For example, CBF might SEEM like it helps you, but really it only helps you because you are more confident when using it.
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Geode/Geode.hpp>
+#include <Geode/ui/BasedButtonSprite.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+
+class $modify(MenuLayerHook, MenuLayer) {
+
+	static void startRandomizeCBF() {
+		srand(time(0));
+		Mod* cbf = Loader::get()->getLoadedMod("syzzi.click_between_frames");
+		cbf->setSettingValue("soft-toggle", rand() % 2 >= 1);
+		FLAlertLayer::create(
+			"CBF randomized!",
+			"You can check whether it is enabled or disabled by going to CBF's geode settings.",
+			"OK"
+		)->show();
+	}
+
+	void buttonPressed(CCObject*) {
+		geode::createQuickPopup(
+			"Randomize CBF?",
+			"This will randomly turn on or off the \"Disable CBF\" setting in the Geode settings for CBF.",
+			"Cancel", "Continue",
+			[](auto, bool btn2) {
+				if (btn2) {
+					startRandomizeCBF();
+				}
+			}
+		);
+	}
+
+	bool init() {
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		auto myButton = CCMenuItemSpriteExtra::create(
+			CCSprite::create("placebo_cbf_logo.png"_spr),
+			this,
+			menu_selector(MenuLayerHook::buttonPressed)
+		);
+
+		auto menu = this->getChildByID("side-menu");
+		menu->addChild(myButton);
+
+		menu->updateLayout();
+		return true;
+	}
+};
